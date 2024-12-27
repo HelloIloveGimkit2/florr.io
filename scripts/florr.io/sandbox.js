1048a1bc3227f32e3f1fbd0bbb12e09a6e614840
kit2d2 <cat@fatherless.dad>
2024-12-27 10:08:18 -0800

    Update sandbox.js

--- scripts/florr.io/sandbox.js
+++ scripts/florr.io/sandbox.js
@@ -7,14 +7,14 @@
 (async () => {
 
 	const currentVersionHash = (await (await fetch("https://florr.io")).text()).match(/const\sversionHash\s=\s"(.*)";/)[1];
-	if (currentVersionHash !== "1a715497781b4f2b1884a9285234ef1ddb9d5463") {
+	if (currentVersionHash !== "2bde17fce5778bfa0aedc3e8325a4207afa8eca1") {
 		console.error("Versionhash error. (Tell this to kit2d2 on discord)");
 		return;
 	}
 
 	const kMaxRarities = 9;
-	const kMaxPetals = 91;
-	const petalInventoryBaseAddress =  2508960;
+	const kMaxPetals = 92;
+	const petalInventoryBaseAddress = 3107840;
 
 	for (let petalIndex = 1; petalIndex <= kMaxPetals; petalIndex++) {
 		for (let rarityIndex = 0; rarityIndex < kMaxRarities; rarityIndex++) {
