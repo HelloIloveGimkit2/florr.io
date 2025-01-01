7da1297cd6f80ab89e9fea1ca52c43e179125c98
cat2d2 <152820768+kit2d2alt@users.noreply.github.com>
2024-12-31 17:38:06 -0800

    test

--- scripts/florr.io/memory-editing/wip.js
+++ scripts/florr.io/memory-editing/wip.js
@@ -5,6 +5,15 @@
 	const kMaxMOBs = 69;
 	const mobGalleryBaseAddress = 18569716;
 	const allowedMobs = [
+        63, // Gambler
+		67, // Nazar Beetle
+		65, // Magic Firefly
+		18, // Square
+		68, // Worm
+		20, // Yellow Ladybug
+        57, // Dummy
+        32, // Digger
+        27 // Bumble beeeeeeee
 	];
 
 	for (let mobIndex = 1; mobIndex <= kMaxMOBs; mobIndex++) {
